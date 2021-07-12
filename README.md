446022363991628e16b852ab7774c45a3d01b1ee
致良知 <2782740594@qq.com>
2021-07-12 22:12:45 +0800

    创建分支,完整使用swaager

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Spring Boot集成Mybatis，MybatisGeneator和swagger
 sql文件夹中的有创建数据库和表的sql脚本
 
 博客地址：https://www.cnblogs.com/FireworksEasyCool/p/11133827.html
+
+
+
+运行程序，浏览器打开：http://localhost:8080/swagger-ui.html
+
--- src/main/java/com/example/demo/web/UserController.java
+++ src/main/java/com/example/demo/web/UserController.java
@@ -1,60 +1,61 @@
 package com.example.demo.web;
 
-import java.util.List;
-import java.time.*;
-
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.User;
-import com.example.demo.mapper.UserMapper;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
 
-@Api(description = "用户接口")
+@Api(value = "类API注解_value", tags = "类API注解_tags", description = "用户接口")
 @RestController
 public class UserController {
-	
-	@Autowired
+
+    @Autowired
     private UserMapper userMapper;
-    
-	@ApiOperation(value = "获取所有用户信息" ,  notes = "返回所有用户信息")
-	@RequestMapping(value = "/getUsers", method=RequestMethod.GET)
-	public List<User> getUsers() {
-		List<User> users=userMapper.selectAll();
-		return users;
-	} 
-    
-    @ApiOperation(value = "根据ID获取用户信息" ,  notes = "返回单个用户信息")
-    @RequestMapping(value = "/getUser", method=RequestMethod.GET)
+
+    @ApiOperation(value = "获取所有用户信息", notes = "返回所有用户信息")
+    @RequestMapping(value = "/getUsers", method = RequestMethod.GET)
+    public List<User> getUsers() {
+        List<User> users = userMapper.selectAll();
+        return users;
+    }
+
+    @ApiOperation(value = "根据ID获取用户信息", notes = "返回单个用户信息")
+    @RequestMapping(value = "/getUser", method = RequestMethod.GET)
     public User getUser(Integer id) {
-    	User user=userMapper.selectByPrimaryKey(id);
+        User user = userMapper.selectByPrimaryKey(id);
         return user;
     }
-    
-    @ApiOperation(value = "新增用户" ,  notes = "注册")
-    @RequestMapping(value = "/add", method=RequestMethod.POST)
+
+    @ApiOperation(value = "新增用户", notes = "注册")
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
     public void save(User user) {
-        if(user.getCreateTime()==null)
-        user.setCreateTime(LocalDateTime.now(Clock.system(ZoneId.of("Asia/Shanghai"))));
-        if(user.getUpdateTime() == null)
-        user.setUpdateTime(LocalDateTime.now(Clock.system(ZoneId.of("Asia/Shanghai"))));
-    	userMapper.insert(user);
+        if (user.getCreateTime() == null)
+            user.setCreateTime(LocalDateTime.now(Clock.system(ZoneId.of("Asia/Shanghai"))));
+        if (user.getUpdateTime() == null)
+            user.setUpdateTime(LocalDateTime.now(Clock.system(ZoneId.of("Asia/Shanghai"))));
+        userMapper.insert(user);
     }
-    
-    @ApiOperation(value = "更新用户信息" ,  notes = "更改用户信息")
-    @RequestMapping(value = "update", method=RequestMethod.PUT)
+
+    @ApiOperation(value = "更新用户信息", notes = "更改用户信息")
+    @RequestMapping(value = "update", method = RequestMethod.PUT)
     public void update(User user) {
-    	userMapper.updateByPrimaryKey(user);
+        userMapper.updateByPrimaryKey(user);
     }
-    
-    @ApiOperation(value = "根据ID删除用户" ,  notes = "删除用户信息")
-    @RequestMapping(value = "/delete/{id}", method=RequestMethod.DELETE)
+
+    @ApiOperation(value = "根据ID删除用户", notes = "删除用户信息")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable("id") Integer id) {
-    	userMapper.deleteByPrimaryKey(id);
+        userMapper.deleteByPrimaryKey(id);
     }
-    
+
 }
