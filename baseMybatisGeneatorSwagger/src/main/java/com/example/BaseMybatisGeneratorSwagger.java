2974ece1e16979dfbbf8b41d64371a14a16a4b2c
致良知 <2782740594@qq.com>
2021-07-14 23:02:55 +0800

    修复mybatis.genertor.core依赖问题

--- baseMybatisGeneatorSwagger/src/main/java/com/example/BaseMybatisGeneratorSwagger.java
+++ baseMybatisGeneatorSwagger/src/main/java/com/example/BaseMybatisGeneratorSwagger.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import com.example.demo.DemoApplication;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,8 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.example.demo.mapper")
 public class BaseMybatisGeneratorSwagger {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BaseMybatisGeneratorSwagger.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BaseMybatisGeneratorSwagger.class, args);
+    }
 
 }
